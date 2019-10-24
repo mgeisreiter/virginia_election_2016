0b6b4a44bf4a58f8c56c9dd052b8d5ef36a57a82
Megan <megangeisreiter@gmail.com>
2019-10-24 18:47:48 -0400

    updated map

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ app.layout = html.Div([
     html.Br(),
     dcc.Graph(id='display-value'),
     html.Br(),
-    html.A('Code on Github', href='https://github.com/austinlasseter/virginia_election_2016'),
+    html.A('Code on Github', href='https://github.com/mgeisreiter/virginia_election_2016'),
     html.Br(),
     html.A('Data Source', href='https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LYWX3D')
 ])
